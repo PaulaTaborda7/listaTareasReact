7e0efd8a487aba5e32f2d84849ded5037ca38fbc
PaulaTaborda7 <ptabordam@unal.edu.co>
2023-11-28 17:52:25 -0500

    Another view to see the pdf of the conceptual questions

--- aplicacion-todo-list-react/src/App.js
+++ aplicacion-todo-list-react/src/App.js
@@ -1,27 +1,45 @@
 import './App.css';
-import gaiaLogo from './images/gaia-logo.png'
+import gaiaLogo from './images/gaia-logo.png';
 import ListaDeTareas from './components/ListaDeTareas';
+import VistaPDF from './components/VistaPDF';
+import { Route, Routes, Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <div className='gaia-logo-contenedor'>
-        <img
-          src={gaiaLogo}
-          className="gaia-logo"
-          title="Logo de GAIA - Grupo de Ambientes Inteligentes Adaptativos" />
-      </div>
-      <div className='boton-conceptos-contenedor'>
-        <button className='conceptos-boton'>Ver punto A - preguntas conceptuales</button>
-      </div>
+
       <div className="aplicacion-tareas">
-        {/* Now, the code of to-do list */}
-        <div className="todo-list">
-          <h1>
-            Mi To-do List 🍎
-          </h1>
-          <ListaDeTareas /> {/*Usamos el componente ListaDeTareas*/}
+        <Routes>
+          <Route path="/" element={<TodoList />} />
+          <Route path="/insertar-pdf" element={<VistaPDF />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+function TodoList() {
+  return (
+    <>
+      <div>
+        <div className='gaia-logo-contenedor'>
+          <img
+            src={gaiaLogo}
+            className="gaia-logo"
+            title="Logo de GAIA - Grupo de Ambientes Inteligentes Adaptativos"
+          />
         </div>
+
+        <div className='boton-conceptos-contenedor'>
+          <Link to="/insertar-pdf">
+            <button className='conceptos-boton'>Ver punto A - preguntas conceptuales</button>
+          </Link>
+        </div>
+      </div>
+
+      <div className="todo-list">
+        <h1>Mi To-do List 🍎</h1>
+        <ListaDeTareas />
       </div>
     </>
   );
---
+++ aplicacion-todo-list-react/src/components/VistaPDF.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VistaPDF = () => {
+  return (
+    <div>
+      {/* Aquí puedes implementar la lógica para insertar el archivo PDF */}
+      <h2>Insertar PDF aquí</h2>
+    </div>
+  );
+}
+
+export default VistaPDF;
